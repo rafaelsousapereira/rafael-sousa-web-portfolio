cdeb51a2187172cb518cf97e4d12ee1e90439927
rafaelsousapereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-04-05 02:03:41 -0300

    feat: add initial setup and add header component

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import { Header } from "./components/header"
+
+export const App = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+  return (
+      <header>
+        <nav>
+          <a href="/about">Sobre</a>
+          <a href="project">Projetos</a>
+        </nav>
+      </header>
+  )
+}
