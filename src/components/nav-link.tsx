240a31a44f97afb6b69692871c939ecc698cca80
rafaelsousapereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-04-09 22:47:04 -0300

    fix: change font of link

--- src/components/nav-link.tsx
+++ src/components/nav-link.tsx
@@ -4,6 +4,6 @@ interface NavLinkProps extends ComponentProps<"a"> {}
 
 export const NavLink = (props: NavLinkProps) => {
   return (
-    <a {...props} className="font-medium text-sm">{props.children}</a>
+    <a {...props} className="font-semibold text-xl">{props.children}</a>
   )
 }
