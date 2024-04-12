f7b60111c594f46cc91597a4e1f21c4cd338939f
rafaelsousapereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-04-11 21:20:47 -0300

    refactor: remove fragment tag

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -2,20 +2,18 @@ import { ButtonGroup } from "./buttons/button-group"
 
 export const Home = () => {
   return (
-    <>
-      <div className="flex flex-col mx-12 gap-10 text-violet-300">
-        <section>
-          <p className="flex flex-col font-medium text-2xl mt-16 pt-9 uppercase">
-            Rafael 
-            <span className="font-black text-4xl">Sousa Pereira</span>
-          </p>
-          <p className="text-sm text-left">Desenvolvedor Full-Stack Freelancer</p>
-        </section>
+    <div className="flex flex-col mx-12 gap-10 text-violet-300">
+      <section>
+        <p className="flex flex-col font-medium text-2xl mt-16 pt-9 uppercase">
+          Rafael 
+          <span className="font-black text-4xl">Sousa Pereira</span>
+        </p>
+        <p className="text-sm text-left">Desenvolvedor Full-Stack Freelancer</p>
+      </section>
 
-        <nav className="flex gap-3">
-          <ButtonGroup>Baixe meu CV</ButtonGroup>
-        </nav>
-      </div>
-    </>
+      <nav className="flex gap-3">
+        <ButtonGroup>Baixe meu CV</ButtonGroup>
+      </nav>
+    </div>
   )
 }
