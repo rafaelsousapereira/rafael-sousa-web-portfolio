33ac112869c571091c6a79ab1ae17adfde5b9951
rafaelsousapereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-04-17 21:35:18 -0300

    feat: add border 2px

--- src/components/buttons/button.tsx
+++ src/components/buttons/button.tsx
@@ -5,7 +5,7 @@ interface ButtonProps extends ComponentProps<"button"> {}
 export const Button = (props: ButtonProps) => {
   return (
       <button {...props} 
-        className="font-extrabold border hover:text-violet-600 border-violet-300 hover:border-violet-600 p-2 rounded w-48 hover:bg-zinc-50/5 transition" 
+        className="font-extrabold border-2 hover:text-violet-600 border-violet-300 hover:border-violet-600 p-2 rounded w-48 hover:bg-zinc-50/5 transition" 
       > 
         {props.children}
       </button>
