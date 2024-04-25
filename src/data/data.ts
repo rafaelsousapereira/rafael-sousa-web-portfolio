ed9cf1b5c5ae6784d5dba67476721430ee112e63
rafaelsousapereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-04-25 00:54:51 -0300

    feat: add router configurations

--- src/data/data.ts
+++ src/data/data.ts
@@ -13,6 +13,9 @@ type MetadataType = {
       linkedin: string,
       github: string
     }
+  },
+  about: {
+    description: string
   }
 }
 
@@ -31,5 +34,20 @@ export const metadata: MetadataType = {
       linkedin: 'https://www.linkedin.com/in/rafael-sousa-pereira-/',
       github: 'https://github.com/rafaelsousapereira/'
     }
+  },
+  about: {
+    description: `
+      'Apaixonado por desenvolvimento Web e música, formado pela Universidade São Francisco (USF), de Campinas - Campus Swift em 2021,\n
+        
+      Com experiência na área de logística, onde trabalhei por quase 10 anos, em 2021 fiz minha transição de carreira para área de tecnologia, onde já tive experiência como desenvolvedor trainee, júnior e atualmente trabalho como desenvolvedor web full stack freelancer.\n
+
+      Habilidade em HTML, CSS, JavaScript e Java; Com objetivo de criar designers minimalistas, responsivos, criar API's robustas e escalaveis.\n
+      
+      Experiência no uso de frameworks Front-end, como ReactJS e Back-end, como Spring Boot e NodeJS e também usando TypeScript como suporte adicional.\n
+      
+      Proativo, sempre buscando novos desafios e conhecimentos.\n
+
+      Sempre mantendo-me atualizado com as tendências do mercado e tecnológicas mais recentes.
+    `
   }
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './app.tsx'
 import './index.css'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Home } from './components/home/home.tsx'
+import { About } from './components/about.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/about",
+    element: <About />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
