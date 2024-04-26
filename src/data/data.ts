60433347671d850c7fa2b20573b6867a3265d2c9
rafaelsousapereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-04-25 22:40:21 -0300

    fix: change about description

--- src/data/data.ts
+++ src/data/data.ts
@@ -40,11 +40,11 @@ export const metadata: MetadataType = {
   about: {
     titles: ["Sobre mim", "Experiência Profissional"],
     description: `
-      Apaixonado por desenvolvimento web e música boa, formado pela Universidade São Francisco (USF) de Campinas - Campus Swift, em 2021.
-      Com experiência na área de logística, onde trabalhei por quase 10 anos, em 2021 fiz a transição de carreira para área de tecnologia, adquirindo experiências como, desenvolvedor trainee, júnior e atualmente como freelancer, com foco no desenvolvimento web, Web Services (Serviços Web) e API's REST/SOAP.
+      Apaixonado por desenvolvimento web e música boa, formado em Análise e Desenvolvimento de Sistemas pela Universidade São Francisco (USF) de Campinas - Campus Swift, em 2021.
+      Com experiência na área de logística onde trabalhei por quase 10 anos, em 2021 fiz a transição de carreira para área de tecnologia, adquirindo experiências como, desenvolvedor trainee, júnior e atualmente como freelancer, com foco no desenvolvimento web, API's REST/SOAP e Web Services (Serviços Web).
       Habilidade em HTML, CSS, JavaScript e Java; Com objetivo de criar designers minimalistas, responsivos, criar API's robustas e escalaveis.
-      Experiência em Java, Spring Boot e Node.js, como Frameworks para Back-End e para Front-End com React.js, Styled Components e Tailwind CSS e utilizo TypeScript como suporte adicional.
-      Proativo, sempre buscando novos desafios e conhecimentos, me mantendo atualizado com as tendências do mercado e tecnologias mais recentes, com muita vontade de aprender.
+      Experiência em Java, Spring Boot e Node.js, em frameworks para Back-End, em Front-End com React.js, Styled Components, Tailwind CSS e Figma e utilizo TypeScript como suporte adicional.
+      Proativo, sempre buscando novos desafios e conhecimentos, me mantendo atualizado com as tendências do mercado e tecnologias mais recentes e com muita vontade de aprender.
     `,
     experience: [
       "Desenvolvedor Web Full Stack, Freelancer - (11/2023-Atual)", 
