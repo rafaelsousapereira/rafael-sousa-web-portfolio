43bf636818b14ab5350a5c0288839d08b5077418
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-05-06 23:15:10 -0300

    fix: run build

--- src/components/buttons/button-home.tsx
+++ src/components/buttons/button-home.tsx
@@ -6,7 +6,7 @@ export const ButtonHome = () => {
     <>
       <Link to="/" className="flex gap-1 fixed bottom-0 left-0 mx-10 my-5 text-base font-extrabold bg-zinc-200/60 p-1 rounded text-zinc-700 hover:bg-violet-600/60 hover:text-violet-300"
         >
-        <SquareArrowLeft /> Home
+        <SquareArrowLeft /> Home 
       </Link>
     </>
   )
