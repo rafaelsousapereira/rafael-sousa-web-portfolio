cd83a3fd4c630e432b7776e01236f243cd25a509
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-05-06 23:02:39 -0300

    fix: remove unused comment

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -38,11 +38,6 @@ export const About = () => {
       </div>
 
       <ButtonHome />
-
-      {/* <Link to="/" className="flex gap-1 fixed bottom-0 left-0 mx-10 my-5 text-base font-extrabold bg-zinc-200/60 p-1 rounded text-zinc-700 hover:bg-violet-600/60 hover:text-violet-300"
-        >
-        <SquareArrowLeft /> Home
-      </Link> */}
     </>
   )
 }
