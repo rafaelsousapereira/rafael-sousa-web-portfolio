b649a746dcfd04fa638025067e8ad11225934a56
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-05-06 23:29:00 -0300

    fix: remove vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 
 import { App } from './app'
-import { SpeedInsights } from '@vercel/speed-insights/next'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <SpeedInsights />
   </React.StrictMode>,
 )
