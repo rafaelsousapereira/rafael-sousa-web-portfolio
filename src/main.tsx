1006247d7dbae8655b951b175f20a0726c862a36
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-05-06 23:24:47 -0300

    feat: add vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 
 import { App } from './app'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <SpeedInsights />
   </React.StrictMode>,
 )
