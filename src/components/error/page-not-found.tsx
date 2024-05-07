d2395140b0aae0251df7f5b0a17e2cfe925129c2
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-05-06 23:54:12 -0300

    fix: remove button

--- src/components/error/page-not-found.tsx
+++ src/components/error/page-not-found.tsx
@@ -1,4 +1,3 @@
-import { ButtonHome } from "../buttons/button-home"
 
 export const PageNotFound = () => {
   return (
@@ -6,7 +5,6 @@ export const PageNotFound = () => {
       <p className="flex justify-center mt-5 text-zinc-300 font-black text-9xl">404</p>
       <p className="flex justify-center mt-5 text-zinc-400 font-black text-6xl">Page not found!</p>
 
-      <ButtonHome />
     </>
   )
 }
