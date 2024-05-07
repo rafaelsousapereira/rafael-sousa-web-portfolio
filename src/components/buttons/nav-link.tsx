75084c1dfb9a02a9ba90fc12d5194f45c28e22ad
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-05-07 19:02:25 -0300

    feat: add icon at menu

--- src/components/buttons/nav-link.tsx
+++ src/components/buttons/nav-link.tsx
@@ -4,7 +4,7 @@ interface NavLinkProps extends LinkProps {}
 
 export const NavLink = (props: NavLinkProps) => {
   return (
-    <Link className="p-2 rounded font-semibold text-xl text-gray-500 hover:text-violet-800 transition"
+    <Link className="flex gap-3 p-2 rounded font-semibold text-xl text-gray-500 hover:text-violet-800 transition"
       {...props}
     >
       {props.children}
--- src/components/header/navbar.tsx
+++ src/components/header/navbar.tsx
@@ -1,20 +1,21 @@
 import { ComponentProps } from "react";
 import { NavLink } from "../buttons/nav-link";
+import { Home, User } from "lucide-react";
 
 interface NavBarProps extends ComponentProps<"ul"> {}
 
 export const NavBar = (props: NavBarProps) => {
   
   const routes = [
-    { url: "/", label: "Home" },
-    { url: "/about", label: "Sobre mim" },
+    { url: "/", label: "Home", icon: <Home /> },
+    { url: "/about", label: "Sobre", icon: <User /> },
   ]
 
   return (
     <ul {...props}>
       {routes.map((item) => (
         <li key={item.url}>
-          <NavLink to={item.url}>{item.label}</NavLink>
+          <NavLink to={item.url}>{item.icon} {item.label}</NavLink>
         </li>
       ))}
     </ul>
