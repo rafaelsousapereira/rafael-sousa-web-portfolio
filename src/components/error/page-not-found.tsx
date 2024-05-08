10eb7ae26f68e73973666e3c74d4e6333e57f7e6
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-05-07 23:02:20 -0300

    rafactor: adjust page not found

--- src/components/error/page-not-found.tsx
+++ src/components/error/page-not-found.tsx
@@ -1,12 +1,21 @@
+import { Construction } from "lucide-react"
 
 export const PageNotFound = () => {
   return (
     <>
-      <p className="flex justify-center items-center mt-16 text-zinc-300 font-black text-6xl">Erro 404</p>
-      <p className="flex justify-center items-center mt-5 max-[320px]:text-center text-zinc-400 font-black text-4xl">
-        Página não encontrada.
-      </p>
-      <p className="mt-5 text-center max-[320px]:text-center text-zinc-400 font-black text-3xl">Tente novamente.</p>
+      <div className="text-center mt-10 text-zinc-300 font-extrabold text-4xl">
+        <span className="flex justify-center mb-4">
+          <Construction size={68} />
+        </span>
+
+        <p>
+          Página em desenvolvimento.
+        </p>
+        
+        <p className="text-zinc-500 mt-4">
+          Tente novamente mais tarde.
+        </p>
+      </div>
     </>
   )
 }
