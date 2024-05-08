005520aa2e62c13299d39a1553bb8c771392db18
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-05-07 22:29:48 -0300

    rafactor: add spaces nav menu

--- src/components/buttons/nav-menu.tsx
+++ src/components/buttons/nav-menu.tsx
@@ -8,7 +8,7 @@ export const NavMenu = (props: NavMenuProps) => {
       className={
         (data) => data.isActive 
           ? "flex gap-1 p-1 mx-4 font-semibold text-xl text-gray-400 border-solid border-b-2 border-violet-800 max-[768px]:text-gray-600 max-[768px]:flex max-[768px]:my-4" 
-          : "flex gap-1 p-1 mx-4 font-semibold text-xl text-gray-500 hover:text-violet-800 transition"
+          : "flex gap-1 p-1 mx-4 font-semibold text-xl text-gray-500 hover:text-violet-800 transition max-[768px]:text-gray-600 max-[768px]:flex max-[768px]:my-4"
       }
       {...props}
     >
