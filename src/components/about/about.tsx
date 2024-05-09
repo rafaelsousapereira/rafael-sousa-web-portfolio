552cd82e756636dbf399233c339f56def898ca5a
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-05-09 15:15:39 -0300

    fix: remove spaces

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -5,13 +5,10 @@ export const About = () => {
 
   return (
     <>
-      <div className="flex flex-col justify-center items-center gap-8 text-zinc-300 m-12"
-      >
-        <p className="text-3xl font-extrabold uppercase"
-        >
+      <div className="flex flex-col justify-center items-center gap-8 text-zinc-300 m-12">
+        <p className="text-3xl font-extrabold uppercase">
           {metadata.about.titles[0]}
         </p>
-
         <p className="text-2xl text-center max-[425px]:text-xl">
           {metadata.about.description}
         </p>
