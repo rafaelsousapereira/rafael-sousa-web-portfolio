95887238734cbde48d8ca342da9c4e5e4f58d1c7
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-05-09 16:39:59 -0300

    fix: changed about

--- src/data/data.ts
+++ src/data/data.ts
@@ -40,9 +40,7 @@ export const metadata: MetadataType = {
   about: {
     titles: ["Sobre mim", "Experiência Profissional"],
     description: `
-      Apaixonado por desenvolvimento web e música boa, formado em Análise e Desenvolvimento de Sistemas pela Universidade São Francisco (USF) de Campinas em 2021.
-      Com experiência na área de logística onde trabalhei por 8 anos, em 2021 iniciei minha transição de carreira para área de tecnologia onde atuei como desenvolvedor trainee, júnior e atualmente como freelancer autônomo, com foco no desenvolvimento web.
-      Com objetivo de criar designers minimalistas, responsivos e criar APIs robustas e escalaveis utilizo a tecnologias para resolver problemas e ajudar pessoas e comunidades na minha região.
+      Apaixonado por desenvolvimento web e música, formado em Análise e Desenvolvimento de Sistemas pela Universidade São Francisco (USF) de Campinas em 2021 iniciei minha transição de carreira para área de tecnologia onde atuei como desenvolvedor trainee, júnior e atualmente como freelancer autônomo, com foco no desenvolvimento web. Com objetivo de criar designers minimalistas, responsivos e criar APIs robustas e escalaveis utilizo a tecnologias para resolver problemas, ajudar pessoas e comunidades na minha região.
     `,
     experience: [
       "Desenvolvedor Web          Freelancer (09/2023 - Atualmente)", 
