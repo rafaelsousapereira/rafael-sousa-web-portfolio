e7f2a5181fd88d8ebbc7482a60fe7723174ad02f
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-07-11 10:38:50 -0300

    fix: add media queries

--- src/components/contact/button-component.tsx
+++ src/components/contact/button-component.tsx
@@ -7,7 +7,7 @@ export const ButtonComponent = (buttonProps: ButtonComponentProps) => {
   return (
     <div>
       <button {...buttonProps}
-        className="flex text-white bg-violet-700 hover:bg-violet-800 focus:ring-4 focus:outline-none focus:ring-violet-300 font-semibold rounded-lg text-lg w-full sm:w-auto px-5 py-2.5 text-center dark:bg-violet-600 dark:hover:bg-violet-700 dark:focus:ring-violet-800"
+        className="flex text-white bg-violet-700 hover:bg-violet-800 focus:ring-4 focus:outline-none focus:ring-violet-300 font-semibold rounded-lg text-lg w-full sm:w-auto px-5 py-2.5 text-center dark:bg-violet-600 dark:hover:bg-violet-700 dark:focus:ring-violet-800  max-sm:justify-center"
       >
         <Send
           className="flex justify-center my-1 mr-1"
