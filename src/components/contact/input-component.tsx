3b01a92f6521616728f7864262828d576a8b1cd1
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-07-12 14:41:24 -0300

    feat: add forward ref at inputs data

--- src/components/contact/input-component.tsx
+++ src/components/contact/input-component.tsx
@@ -1,16 +1,22 @@
-import { ComponentProps } from "react";
+import { ComponentProps, ForwardedRef, forwardRef } from "react";
 
 interface InputComponentProps extends ComponentProps<"input"> {}
 
-export const InputComponent = (inputProps: InputComponentProps) => {
+export const InputComponent = forwardRef((props: InputComponentProps, 
+  ref: ForwardedRef<HTMLInputElement>
+) => {
+
+  const { ...inputProps } = props
+
   return (
     <div className="relative z-0 w-full mb-5 group">
-      <input {...inputProps}
+      <input {...inputProps} 
         className="block py-2.5 px-0 w-full text-lg text-gray-400 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-violet-500 focus:outline-none focus:ring-0 focus:border-violet-600 peer" type={inputProps.type} 
         name={inputProps.name}
         id={inputProps.id}
         placeholder=" " 
         required 
+        ref={ref}
       />
       <label
         className="peer-focus:font-semibold absolute text-lg text-gray-500 dark:text-gray-400 duration-300 transform  -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-violet-600 peer-focus:dark:text-violet-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
@@ -20,4 +26,4 @@ export const InputComponent = (inputProps: InputComponentProps) => {
       </label>
     </div>
   )
-}
+})
--- src/components/contact/textarea-componet.tsx
+++ src/components/contact/textarea-componet.tsx
@@ -1,8 +1,13 @@
-import { ComponentProps } from "react"
+import { ComponentProps, ForwardedRef, forwardRef } from "react"
 
 interface TextAreaComponentProps extends ComponentProps<"textarea">{}
 
-export const TextAreaComponent = (textareaProps: TextAreaComponentProps) => {
+export const TextAreaComponent = forwardRef((props: TextAreaComponentProps, 
+  ref: ForwardedRef<HTMLTextAreaElement>
+) => {
+  
+  const { ...textareaProps } = props
+
   return (
     <div className="relative z-0 w-full mb-5 group">
       <textarea {...textareaProps}
@@ -10,6 +15,7 @@ export const TextAreaComponent = (textareaProps: TextAreaComponentProps) => {
         id={textareaProps.id} 
         placeholder=" " 
         required
+        ref={ref}
       />
       <label 
         className="peer-focus:font-semibold absolute text-lg text-gray-500 dark:text-gray-400 duration-300 transform  -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-violet-600 peer-focus:dark:text-violet-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6" htmlFor="event-message"
@@ -18,4 +24,4 @@ export const TextAreaComponent = (textareaProps: TextAreaComponentProps) => {
       </label>
     </div>
   )
-}
+})
